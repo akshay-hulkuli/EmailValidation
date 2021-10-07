b2d492ba42d40ee0b56078c999e629ccf1c76b3a
Akshaya HP <akshayhulkuli@gmail.com>
2021-10-07 22:39:46 +0530

    REFACTORED: regex to validate Top level domain

--- EmailValidator.js
+++ EmailValidator.js
@@ -1,10 +1,9 @@
 let emailArray = new Array();
-emailArray.push("abc@bridgelabz");
-emailArray.push("a-bcd.bridgelabz");
-emailArray.push("1abc");
+emailArray.push("abc@bridgelabz.co");
+emailArray.push("a-bcd.bridgelabz-co");
 
 function emailValidator(email){
-    let emailRegex = RegExp("(^[a-zA-Z][a-zA-Z0-9_-]*[a-z|A-Z|0-9])(@[a-zA-Z0-9]*)");
+    let emailRegex = RegExp("(^[a-zA-Z][a-zA-Z0-9_-]*[a-z|A-Z|0-9])(@[a-zA-Z0-9]*)(.)([a-z]{2,})");
     if(emailRegex.test(email))
         console.log("   passed");
     else throw '    email is not valid';
