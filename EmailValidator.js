8c71c4dc1c5dec99bdf30625becde9c01829b4a2
Akshaya HP <akshayhulkuli@gmail.com>
2021-10-07 22:29:11 +0530

    REFACTORED: regex to validate domain name

--- EmailValidator.js
+++ EmailValidator.js
@@ -1,10 +1,10 @@
 let emailArray = new Array();
-emailArray.push("abc");
-emailArray.push("a-bcd");
+emailArray.push("abc@bridgelabz");
+emailArray.push("a-bcd.bridgelabz");
 emailArray.push("1abc");
 
 function emailValidator(email){
-    let emailRegex = RegExp("(^[a-zA-Z][a-zA-Z0-9_-]*[a-z|A-Z|0-9])");
+    let emailRegex = RegExp("(^[a-zA-Z][a-zA-Z0-9_-]*[a-z|A-Z|0-9])(@[a-zA-Z0-9]*)");
     if(emailRegex.test(email))
         console.log("   passed");
     else throw '    email is not valid';
