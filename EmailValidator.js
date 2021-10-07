ef8909b0154ce4d93fb90c821e06261f374f6e7f
Akshaya HP <akshayhulkuli@gmail.com>
2021-10-07 22:43:52 +0530

    REFACTORED: regex to validate optional username part

--- EmailValidator.js
+++ EmailValidator.js
@@ -1,9 +1,9 @@
 let emailArray = new Array();
-emailArray.push("abc@bridgelabz.co");
+emailArray.push("abc.xyz@bridgelabz.co");
 emailArray.push("a-bcd.bridgelabz-co");
 
 function emailValidator(email){
-    let emailRegex = RegExp("(^[a-zA-Z][a-zA-Z0-9_-]*[a-z|A-Z|0-9])(@[a-zA-Z0-9]*)(.)([a-z]{2,})");
+    let emailRegex = RegExp("([a-zA-Z][a-zA-Z0-9_-]*[a-z|A-Z|0-9])(([+-_.][a-zA-Z0-9]*)?)(@[a-zA-Z0-9]*)([.])([a-z]{2,})");
     if(emailRegex.test(email))
         console.log("   passed");
     else throw '    email is not valid';
